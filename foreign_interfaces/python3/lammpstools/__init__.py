dc86f256725697199c8c9ae045dc3db0b502dbf7
Stefan Paquay <stefanpaquay@gmail.com>
2017-05-19 13:47:42 -0400

    Improved __init__.py

--- foreign_interfaces/python3/lammpstools/__init__.py
+++ foreign_interfaces/python3/lammpstools/__init__.py
@@ -3,8 +3,5 @@ __all__ = [ 'block_data.py', 'test_me.py', 'dump_reader.py' ]
 import sys, os
 sys.path.append( os.path.dirname(__file__) )
 
-print("Path in __init__.py is: ", sys.path)
-
 import block_data
 import dump_reader
-import test
